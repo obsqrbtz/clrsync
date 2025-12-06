c61337de0aed499ca971359ae294343a7a0b0a86
Daniel Dada <doesdeos@gmail.com>
2025-12-07 02:17:28 +0300

    bump ver

--- src/core/version.hpp
+++ src/core/version.hpp
@@ -9,7 +9,7 @@ namespace clrsync::core
 
 constexpr uint8_t VERSION_MAJOR = 0;
 constexpr uint8_t VERSION_MINOR = 1;
-constexpr uint8_t VERSION_PATCH = 0;
+constexpr uint8_t VERSION_PATCH = 1;
 
 const std::string version_string();
 } // namespace clrsync::core
