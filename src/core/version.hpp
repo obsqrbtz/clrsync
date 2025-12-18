613c2c80f557f8a050c265e71d3bbdfbefef491c
Daniel Dada <doesdeos@gmail.com>
2025-12-18 11:51:28 +0300

    build: fix msvc builds

--- src/core/version.hpp
+++ src/core/version.hpp
@@ -6,7 +6,7 @@
 namespace clrsync::core
 {
 
-const std::string GIT_SEMVER = "0.1.6+git.g57c3c55";
+const std::string GIT_SEMVER = "0.1.6+git.g1a1747a";
 
 const std::string version_string();
 } // namespace clrsync::core
