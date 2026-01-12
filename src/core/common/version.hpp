e3cd9cd3626514feb652f492a2adc457507fc6ba
Daniel Dada <doesdeos@gmail.com>
2026-01-12 10:50:51 +0300

    added vscode template

--- src/core/common/version.hpp
+++ src/core/common/version.hpp
@@ -6,7 +6,7 @@
 namespace clrsync::core
 {
 
-const std::string GIT_SEMVER = "1.0.0+git.g5b0599a";
+const std::string GIT_SEMVER = "1.0.1+git.ge256dca";
 
 const std::string version_string();
 } // namespace clrsync::core
